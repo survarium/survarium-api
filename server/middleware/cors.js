b78f554fa5bd578c2bb1953ff64a62e187de63c5
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-01-17 16:18:27 +0500

    CORS multi-hosts

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const config = require('../configs');
+const origins = config.cors.origin.split(';');
 
 module.exports = function (req, res, next) {
-	res.header("Access-Control-Allow-Origin", config.cors.origin || '*');
+	if (!req.headers.origin) {
+		return req.method === 'options' ? res.end() : next();
+	}
+	var origin = origins.indexOf(req.headers.origin);
+	res.header("Access-Control-Allow-Origin", origins[origin] || origins[0]);
 	res.header("Access-Control-Allow-Credentials", true);
 	res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE');
 	res.header("Access-Control-Allow-Headers", 'Origin, Content-Type, Accept, Authorization');
