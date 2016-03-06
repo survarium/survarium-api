bb04507b28ec2e11094aa7ce725ba8dc552c947f
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-03-06 05:22:24 +0500

    V2 Init Clan players

--- server/v2/components/clans/ctl.js
+++ server/v2/components/clans/ctl.js
@@ -2,8 +2,13 @@
 
 const Promise = require('bluebird');
 const model   = require('../../../v1/components/clans/model');
+const Players = require('../../../v1/components/players/model');
 const db      = require('../../../v1/lib/db');
 const libLang = require('../../../v1/lib/lang');
+
+exports.id = function (abbr) {
+	return model.findOne({ abbr: abbr }, { _id: 1, abbr: 1 }).lean();
+};
 
 exports.list = function list (options) {
 	options = options || {};
@@ -59,4 +64,136 @@ exports.list = function list (options) {
 		skip: skip,
 		limit: limit
 	});
+};
+
+exports.fetch = function (clan) {
+	var cursor = model
+		.findOne({
+			_id: clan._id
+		});
+
+	cursor.select({
+		_id: 0,
+		players: 0,
+		stats: 0,
+		matches: 0,
+		createdAt: 0,
+		__v: 0
+	});
+
+	return cursor.lean();
+};
+
+exports.players = function list (clan, options) {
+	options = options || {};
+
+	var sort = options.sort || { role: -1 };
+	var limit = 15;
+	var skip = 0;
+
+	if (options.skip) {
+		var __skip = Number(options.skip);
+		if (!isNaN(__skip) && (__skip > 0)) {
+			skip = __skip;
+		}
+	}
+
+	var roleSort = sort.role;
+	if (roleSort) {
+		delete sort.role;
+	}
+
+	var total;
+
+	return model
+		.findOne({ _id: clan._id }, { players: 1 })
+		.lean()
+		.then(function (clan) {
+			var players = clan.players;
+			total = players.length;
+
+			var roles = {};
+
+			var ids = players.map(function (elem) {
+				var id = elem.player;
+				roles[id] = { role: elem.role };
+				return id;
+			});
+
+			if (roleSort) {
+				var Ranks = {
+					commander: 99,
+					assistant: 80,
+					warlord  : 50,
+					soldier  : 10
+				};
+
+				ids = ids.sort(function (a, b) {
+					if (roleSort === '1') {
+						let c = b;
+						b = a;
+						a = c;
+					}
+
+					let w1 = Ranks[roles[a].role] || 0;
+					let w2 = Ranks[roles[b].role] || 0;
+					return w2 - w1;
+				}).slice(skip, skip + limit);
+
+				ids.forEach(function (id, index) {
+					roles[id].index = index;
+				});
+			}
+
+			var cursor = Players
+				.find({ _id: { $in: ids } })
+				.select({
+					clan: 0,
+					clan_meta: 0,
+					stats: 0,
+					skills: 0,
+					ammunition: 0,
+					__v: 0,
+					updatedAt: 0,
+					createdAt: 0
+				});
+
+			if (!roleSort) {
+				cursor
+					.sort(sort)
+					.skip(skip)
+					.limit(limit)
+			}
+
+			return cursor
+				.lean()
+				.then(function (players) {
+					var filtered = players.length;
+
+					var result = [];
+
+					if (roleSort) {
+						players.forEach(function (player) {
+							let id = player._id;
+							player.role = roles[id].role;
+							delete player['_id'];
+							result[roles[id].index] = player;
+						});
+					} else {
+						result = players.map(function (player) {
+							player.role = roles[player._id].role;
+							delete player['_id'];
+							return player;
+						});
+					}
+
+					return {
+						data: result,
+						filtered: filtered,
+						total: total,
+						skip: skip,
+						limit: limit
+					};
+				});
+		});
 };
--- server/v2/components/clans/index.js
+++ server/v2/components/clans/index.js
@@ -23,8 +23,33 @@ router.get('/cw', function (req, res, next) {
 		.catch(next);
 });
 
-router.get('/clans/:abbr');
-router.get('/clans/:abbr/members');
+router.param('abbr', function (req, res, next, value) {
+	ctl
+		.id(value)
+		.then(function (clan) {
+			if (!clan) {
+				return res.status(404).json({ code: 404, message: `Clan '${value}' not found`});
+			}
+			req.clan = clan;
+			next();
+		})
+		.catch(next);
+});
+
+router.get('/:abbr', function (req, res, next) {
+	ctl
+		.fetch(req.clan)
+		.then(res.json.bind(res))
+		.catch(next);
+});
+
+router.get('/:abbr/players', function (req, res, next) {
+	ctl
+		.players(req.clan, req.query)
+		.then(res.json.bind(res))
+		.catch(next);
+});
+
 router.get('/clans/:abbr/matches');
 router.get('/clans/:abbr/cw');
 router.get('/clans/:abbr/cw/matches');
