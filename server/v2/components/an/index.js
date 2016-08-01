78e3205a184faa71ef522b4fa4dbf04b2e53c919
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-08-01 18:59:22 +0500

    An.yandex.ru proxy /count support

--- server/v2/components/an/index.js
+++ server/v2/components/an/index.js
@@ -7,25 +7,37 @@ const config = require('../../../configs');
 const HOST = 'an.yandex.ru';
 
 router.all('*', (req, res, next) => {
-    if (config.env !== 'production') {
-        req.query['page-ref'] = req.query['target-ref'] = req.headers.referer = req.query['page-ref']
+    if (config.env !== 'production' && req.path.match(/^\/page/)) {
+        req.query['page-ref'] = req.query['target-ref'] = req.headers.referer = req.query['target-ref']
             .replace('http:', 'https:')
             .replace('.dev', '.pro')
             .replace(':3000', '');
     }
     
+    let incomingHost = req.headers.host;
     req.headers.host = HOST;
     
-    got.stream(`https://${HOST}${req.path}`,
+    got(`https://${HOST}${req.path}`,
         {
             method: req.method,
             query: req.query,
             retries: 0,
             headers: req.headers
         })
-        .on('error', next)
-        .pipe(res)
-        .on('error', next);
+        .then(an => {
+            let replacer;
+            
+            if (req.secure) {
+                replacer = /an\.yandex\.ru/g;
+                incomingHost = `${incomingHost}${req.baseUrl}`;
+            } else {
+                replacer = /(https:\/\/)?an\.yandex\.ru/g;
+                incomingHost = `${req.protocol}://${incomingHost}${req.baseUrl}`;
+            }
+            
+            res.set(an.headers).send(an.body.replace(replacer, incomingHost));
+        })
+        .catch(next);
 });
 
 module.exports = router;
