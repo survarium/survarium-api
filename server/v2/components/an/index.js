8788a9cfc8891e311ed2c446249670b85d15f905
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-08-04 03:54:34 +0500

    Realtime context.js proxy-patcher

--- server/v2/components/an/index.js
+++ server/v2/components/an/index.js
@@ -1,10 +1,73 @@
 'use strict';
 
 const got = require('got');
+const zlib = require('zlib');
+const Readable = require('stream').Readable;
+
 const router = require('express').Router();
 const config = require('../../../configs');
 
-const HOST = 'an.yandex.ru';
+/*function Cache () {
+    this.cache = {};
+}
+
+Cache.prototype.set = (key, val, ttl) => {
+    this.cache[key] = val;
+    
+    setTimeout(() => {
+        this.cache[key] = null;
+    }, ttl);
+};
+
+Cache.prototype.get = (key) => {
+    return this.cache[key];
+};*/
+
+function patchCookie(hostname, an) {
+    let cookie = an.headers && an.headers['set-cookie'];
+    
+    if (cookie) {
+        let cookieDomain = /domain=an\.yandex\.ru;/;
+        
+        an.headers['set-cookie'] = cookie.forEach(cook => {
+            if (cookieDomain.test(cook)) {
+                an.headers['set-cookie'].push(cook.replace(cookieDomain, `domain=${hostname};`));
+            }
+        });
+    }
+    
+    return an;
+}
+
+function answer(headers, res, an) {
+    let body = new Readable;
+    body.push(an.body);
+    body.push(null);
+    
+    if (an.headers['content-encoding'] === 'gzip') {
+        delete an.headers['content-length'];
+        delete an.headers['content-encoding'];
+        
+        if (headers['accept-encoding'] && headers['accept-encoding'].match(/(gzip|deflate)/)) {
+            let encoding = RegExp.$1;
+            
+            an.headers['content-encoding'] = encoding;
+            res.set(an.headers);
+            
+            switch (encoding) {
+                case 'deflate':
+                    return body.pipe(zlib.createDeflate()).pipe(res);
+                case 'gzip':
+                    return body.pipe(zlib.createGzip()).pipe(res);
+            }
+        }
+        
+    }
+    
+    res.set(an.headers);
+    
+    return body.pipe(res);
+}
 
 router.all('*', (req, res, next) => {
     if (config.env !== 'production' && req.path.match(/^\/page/)) {
@@ -14,39 +77,50 @@ router.all('*', (req, res, next) => {
             .replace(/:\d+/, '');
     }
     
-    let incomingHost = req.headers.host;
+    let headers = req.headers;
+    let incomingHost = headers.host;
     let hostname = req.hostname;
     
-    req.headers.host = HOST;
+    headers.host = 'an.yandex.ru';
     
-    got(`https://${HOST}${req.path}`,
+    headers.pragma = headers['cache-control'] = 'no-cache';
+    
+    delete headers['if-none-match'];
+    delete headers['if-modified-since'];
+    
+    let flow = got(`https://an.yandex.ru${req.path}`,
         {
             method: req.method,
             query: req.query,
             retries: 0,
-            headers: req.headers
-        })
-        /*.then(an => {
-            let replacer = `${req.protocol}://${incomingHost}${req.baseUrl}`;
-            
-            an.body = an.body
-                .replace(/https:\/\/an\.yandex\.ru/g, replacer)
-                .replace(new RegExp(`abuseInfo: "${replacer.replace(/([/.])/g, '\$1')}`, 'g'), 'abuseInfo: "https://an.yandex.ru');
-            
-            return an;
-        })*/
-        .then(an => {
-            let cookie = an.headers && an.headers['set-cookie'];
-            
-            if (cookie) {
-                an.headers['set-cookie'] = cookie.map(cook => cook.replace(/domain=an\.yandex\.ru;/, `domain=${hostname};`));
-            }
-            
-            return an;
-        })
-        .then(an => {
-            return res.set(an.headers).send(an.body);
-        })
+            headers: headers
+        });
+    
+    if (req.path === '/system/context.js') {
+        flow = flow
+            .then(an => {
+                an.body = an.body.replace(/an\.yandex\.ru/g, `${incomingHost}${req.baseUrl}`);
+                
+                if (!req.secure) {
+                    an.body = an.body.replace(/https:/g, 'http:');
+                }
+                
+                return an;
+            });
+    } else if (req.path.match(/\/resource\/context_static/)) {
+        flow = flow
+            .then(an => {
+                an.body = an.body
+                    .replace(/(http(s)?:\/\/)?an\.yandex\.ru/g, `http${req.secure ? 's' : ''}://${incomingHost}${req.baseUrl}`)
+                    .replace(/(jserrlog:)"https:\/\/an([^"]*)"/, '$1"https://an.yandex.ru$2"');
+                
+                return an;
+            });
+    }
+    
+    return flow
+        .then(patchCookie.bind(null, hostname))
+        .then(answer.bind(null, headers, res))
         .catch(next);
 });
 
