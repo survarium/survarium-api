402d0feabc8e0e0cbb68a298ed6ea726e828d7cb
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-08-13 10:31:26 +0300

    Owners count fix

--- server/v2/components/game/ctl.js
+++ server/v2/components/game/ctl.js
@@ -32,7 +32,7 @@ function getProjections(versions, language, thin) {
 		project['is_stack'] = 1;
 		project['category'] = '$item_category';
         project['visual'] = 1;
-        project['owners'] = { $size: '$owners' };
+        project['owners'] = { $size: { '$ifNull': ['$owners', [] ] } };
         project['usage'] = 1;
 	}
 
