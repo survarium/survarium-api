845d5dba48141b2ef97b19da90065cd9a7f44cbe
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-09-15 14:06:21 +0500

    Steam online with proper cache

--- server/v2/components/steam/index.js
+++ server/v2/components/steam/index.js
@@ -4,33 +4,42 @@ const got = require('got');
 
 const router = require('express').Router();
 const config = require('../../../configs').steam;
+const cache  = require('../../../v1/lib/cache');
 
 const API = config.api;
 const ONLINE = `${API}/ISteamUserStats/GetNumberOfCurrentPlayers/v1/`;
-const ONLINE_TTL = 1 * 60 * 1000;
+const ONLINE_TTL = 60;
+const ONLINE_KEY = `STEAM:ONLINE`;
 
-let ONLINE_COUNT = null;
-
-router.get('/online', (req, res, next) => {
-    let send = () => res.json({ count: ONLINE_COUNT });
-    
-    if (ONLINE_COUNT !== null) {
-        return send();
-    }
-    
-    got(ONLINE, {
+function ONLINE_FETCH () {
+    return got(ONLINE, {
         json: true,
         query: {
             appid: config.appid
         },
         retries: 0
     }).then(response => {
-        ONLINE_COUNT = response.body.response.player_count;
-        
-        setTimeout(() => ONLINE_COUNT = null, ONLINE_TTL);
-        
-        send();
-    }).catch(next);
+        return response.body.response.player_count;
+    });
+}
+
+router.get('/online', (req, res, next) => {
+    let send = (count) => res.json({ count: count });
+    
+    cache.get(ONLINE_KEY)
+        .then(count => {
+            if (count !== null) {
+                return Number(count);
+            }
+            
+            return ONLINE_FETCH()
+                .then(count => {
+                    cache.set(ONLINE_KEY, count, 'EX', ONLINE_TTL);
+                    return count;
+                });
+        })
+        .then(count => send(count))
+        .catch(next);
 });
 
 module.exports = router;
